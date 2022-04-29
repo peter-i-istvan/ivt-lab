0a697521b04a6b51d3acfb417cabf4257d6bbe36
peter-istvan <peter.istvan@edu.bme.hu>
2022-04-29 11:47:09 +0200

    Fixed problems indicated by the review on PR#1

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -13,7 +13,7 @@ public class TorpedoStore {
   private double FAILURE_RATE = 0.0; // NOSONAR
 
   private int torpedoCount = 0;
-  private Random generator = new Random();
+  private Random generator = new Random(); // Jlesz teso
 
   public TorpedoStore(int numberOfTorpedos) {
     this.torpedoCount = numberOfTorpedos;
